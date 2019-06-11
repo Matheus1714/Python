96b0945ec8f528a242c58a99b16cc381c78c1199
matheus1714 <matheussilvamartins1714@gmail.com>
2019-06-11 06:25:52 -0300

    numerical integrate

--- CCI/numerical_integration/trapeze.py
+++ CCI/numerical_integration/trapeze.py
@@ -1,5 +1,30 @@
+import matplotlib.pyplot as plt
+from numpy import trapz
+
+def f(x):
+    return 2 + 5*x - 3*x**2 + 6*x**3 - 7*x**4 - 3*x**5 + x**6
+def E(space, inf, sup):
+    F=[]
+    h = (sup-inf)/space
+    for i in range(space):
+        F.append(f(inf+i*h))
+    return F
 def main():
     print("trapeze rule")
+    a = 0
+    b = 1
+    space = 4
+    h = (b-a)/space
+    y = E(space,a,b)
+    Ia = trapz(y,dx=h)
+
+    print(Ia)
+
+    I = int(input("Exact integrate"))
+
+    Et = I - Ia
     
+
+
 if __name__=="__main__":
     main()
